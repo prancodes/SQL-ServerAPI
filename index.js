11f777a02b1d4f40ab4461cf3fab7494a7997630
Pranjal Singh <prancoder@gmail.com>
2025-03-14 19:30:24 +0530

    Update Schema and added SSL

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true  // Enforces secure connection
+  }
 });
 connection.connect((err) => {
   if (err) {
